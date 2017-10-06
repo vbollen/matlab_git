51911ebbb3e1b860943a928f3aa10ac959968150
Viktor Bollen <viktorbollen@gmail.com>
2017-10-06 11:40:10 -0500

    creating a package branch

---
+++ +testpackage/private/private_function.m
@@ -0,0 +1,7 @@
+function y = private_function( x)
+%PRIVATE_FUNCTION Summary of this function goes here
+%   Detailed explanation goes here
+    disp('how did you find me?')
+
+end
+
---
+++ +testpackage/test_function.m
@@ -0,0 +1,7 @@
+function y = test_function(x)
+%TEST_FUNCTION Summary of this function goes here
+%   Detailed explanation goes here
+    y = x * 42;
+    disp(private_function(3))
+end
+
